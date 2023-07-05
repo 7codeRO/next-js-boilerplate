1963dc7ce7fad7a7aa4d8709796e57922511d520
mihaisevencode <mihai.miuta@7code.ro>
2023-07-05 14:25:24 +0300

    [migrate-tailwind] Migrate Datepicker

--- src/components/Form/DatePickerInput/DatePickerInput.tsx
+++ src/components/Form/DatePickerInput/DatePickerInput.tsx
@@ -1,50 +1,32 @@
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import AdapterDayjs from '@mui/lab/AdapterDayjs';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import DatePicker from '@mui/lab/DatePicker';
-import { InputAdornment, TextField } from '@mui/material';
+import Datepicker from 'tailwind-datepicker-react';
 import { useState } from 'react';
-import { useField } from 'formik';
-
+import { FieldMetaProps, useField } from 'formik';
+import { FormField } from '../Form.types';
+import { TextInput } from 'flowbite-react';
 import styles from '/styles/inputs.module.scss';
 
-import { FormField } from '../Form.types';
+const getUserLocale = () => navigator.languages && navigator.languages.length
+  ? navigator.languages[0]
+  : navigator.language;
+
+const optionsForMeta = ({ initialValue }: FieldMetaProps<any>) => ({
+  language: getUserLocale() || 'en-US',
+  defaultDate: initialValue || new Date()
+});
 
 const DatePickerInput = ({ field }: { field: FormField }) => {
+  const [show, setShow] = useState(false);
   const [fieldProps, meta, helpers] = useField(field.name);
-  const [value, setValue] = useState<Date | null>(meta.initialValue);
+
+  const handleChange = (newValue: Date) => {
+    helpers.setValue(newValue?.toISOString(), true);
+  };
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker
-        label={field.label}
-        value={value}
-        onChange={(newValue: Date) => {
-          setValue(newValue);
-          helpers.setValue(newValue?.toISOString(), true);
-        }}
-        renderInput={(params: object) => (
-          <TextField
-            fullWidth
-            {...params}
-            {...fieldProps}
-            margin="dense"
-            id={field.name}
-            error={meta.touched && !!meta.error}
-            helperText={meta.touched && meta.error}
-            InputLabelProps={{ className: styles.label }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <KeyboardArrowDownIcon />
-                </InputAdornment>
-              ),
-              className: styles.input,
-            }}
-          />
-        )}
-      />
-    </LocalizationProvider>
+    <Datepicker options={optionsForMeta(meta)} onChange={handleChange} show={show} setShow={setShow}>
+      <TextInput id={field.name} {...fieldProps} className={styles.input}
+                 helperText={meta.touched && meta.error} />
+    </Datepicker>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./styles/*.{scss,sass}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/tailwind-datepicker-react/dist/**/*.js",
   ],
   theme: {
     extend: {},
