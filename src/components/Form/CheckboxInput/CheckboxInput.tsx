e95366372489f3c9bba9dbc05b9b1d9242d3218f
mihaisevencode <mihai.miuta@7code.ro>
2023-07-06 11:31:42 +0300

    [migrate-tailwind] Remove stylesheet from CheckboxInput

--- src/components/Form/CheckboxInput/CheckboxInput.tsx
+++ src/components/Form/CheckboxInput/CheckboxInput.tsx
@@ -4,8 +4,6 @@ import { useState } from 'react';
 
 import { FormField, FormFieldOption } from '../Form.types';
 
-import styles from './CheckboxInput.module.scss';
-
 const CheckboxInput = ({ field }: { field: FormField }) => {
   const [_, meta, helpers] = useField(field.name);
   const [value, setValue] = useState(meta.initialValue);
@@ -17,7 +15,7 @@ const CheckboxInput = ({ field }: { field: FormField }) => {
       {field.options?.map((option: FormFieldOption, optionIndex: number) => {
         return (
           <div key={optionIndex} className="flex items-center space-x-3">
-            <Checkbox className={styles.label} name={`${field.name}-${optionIndex}`}
+            <Checkbox className={'flex text-sm'} name={`${field.name}-${optionIndex}`}
                       checked={isChecked[optionIndex] || false}
                       onChange={(event) => {
                         const newValue = { ...value };
@@ -27,7 +25,7 @@ const CheckboxInput = ({ field }: { field: FormField }) => {
                         helpers.setValue(newValue);
                       }}
             />
-            <Label className={styles.label}>{option.label}</Label>
+            <Label className={'flex text-sm'}>{option.label}</Label>
           </div>
 
         );
